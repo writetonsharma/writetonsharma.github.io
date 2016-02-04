2bb48ac503ebcb660772696b52dc6531259d2a0f
writetonsharma <writetonsharma@gmail.com>
2016-02-04 14:55:44 +0530

    updated: html refresh on orientation change.

--- TestOrientationChange/js/PreloadJqueryInit.js
+++ TestOrientationChange/js/PreloadJqueryInit.js
@@ -2,7 +2,6 @@ jQuery(document).on("mobileinit", function() {
     jQuery.mobile.autoInitializePage = false;
 });
 window.addEventListener("orientationchange", function() {
-	alert(window.orientation);
     window.location.reload()
 }, false); 
 
