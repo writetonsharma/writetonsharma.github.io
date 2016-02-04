23623d6cea2cd2493098c5942077df2b0d85b499
writetonsharma <writetonsharma@gmail.com>
2016-02-04 14:49:01 +0530

    added alert

--- TestOrientationChange/js/PreloadJqueryInit.js
+++ TestOrientationChange/js/PreloadJqueryInit.js
@@ -2,6 +2,7 @@ jQuery(document).on("mobileinit", function() {
     jQuery.mobile.autoInitializePage = false;
 });
 jQuery(window).on("orientationchange",function(){
+	alert(window.orientation);
     window.location.reload()
 }, false); 
 
