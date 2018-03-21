f5f6a3162f7ebc33c9a090141d032ca2665607ba
writetonsharma <writetonsharma@gmail.com>
2018-03-21 16:50:27 +0530

    deletetd.

--- EmbedHtmlTest/js/PreloadJqueryInit.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).on("mobileinit", function() {
-    jQuery.mobile.autoInitializePage = false;
-});
-window.addEventListener("orientationchange", function() {
-    window.location.reload()
-}, false); 
