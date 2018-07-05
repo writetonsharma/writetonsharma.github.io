eb95f47b58a7fff03f37bd0b7c26c65388dc837d
Naveen <writetonsharma@gmail.com>
2018-07-05 15:33:48 -0500

    changes

---
+++ TestOrientationChange2/js/PreloadJqueryInit.js
@@ -0,0 +1,58 @@
+var currDev;
+var deviceCount = 0;
+devices = {};
+
+jQuery(document).on("mobileinit",function()
+{
+	jQuery.mobile.autoInitializePage=!1
+}),
+window.addEventListener("orientationchange",function()
+{
+	window.location.reload()
+},!1);
+
+getDevice = function(width)
+{
+	var currRange = 0;
+	for(i = 0;i < deviceCount;i++)
+	{
+		if(i == (deviceCount - 1))
+		{
+			currRange++;	//biggest device
+			return currRange;
+		}
+		
+		if(width >= devices[i] 
+			&& width < devices[i + 1])
+		{
+			break;
+		}
+		currRange++;
+	}
+	
+	return currRange;
+}
+
+$( document ).load()
+{	
+	var links = $('link');
+	$(links).each(function() {
+		
+		if($(this).attr('rel') == 'stylesheet' 
+			&& (str = $(this).attr('media')) != undefined)
+		{
+			if(deviceCount == 0)
+			{
+				devices[deviceCount++] = 0;		// starts from 0
+			}
+			
+			var dw = str.match(/\d+/);
+			devices[deviceCount++] = dw[0];
+		}
+	});
+
+	currDev = deviceCount;
+	
+	var width = $(window).width();
+	currDev = getDevice(width);
+}
---
+++ TestOrientationChange2/readme.txt
@@ -0,0 +1,3 @@
+Changed for BCHydro.
+If orientation change, actually changes a device, then only a refresh will occur.
+Otherwise no refresh.
